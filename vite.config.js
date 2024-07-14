1fff103e23e3681178b88a83a9510661a8ed9e3a
Manish Pokhrel <pokhrelmanish@gmail.com>
2024-07-14 12:19:00 +0545

    service worker added

--- vite.config.js
+++ vite.config.js
@@ -10,90 +10,90 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // ==============================||  CONFIGURATION SETTINGS ||============================== //
 export default defineConfig(({ command }) => {
-  const isServe = command === 'serve';
-  const isBuild = command === 'build';
+   const isServe = command === 'serve';
+   const isBuild = command === 'build';
 
-  //common configration for serve && build
-  const config = {
-    base: '/',
-    clearScreen: true,
-    plugins: [
-      react(),
-      jsconfigPaths(),
-      ViteEslint(),
-      timeReporter(),
-      Inspect(),
-      VitePWA({
-        registerType: 'prompt',
-        manifest: {
-          name: 'Shift Pixel',
-          short_name: 'SHF',
-          description: 'Kathmandu Chandol Nepal',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: '1.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: '2.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: '3.png',
-              sizes: '192x192',
-              type: 'image/png'
-            }
-          ]
-        }
-      })
-    ],
-    resolve: {
-      alias: {
-        '@src': path.resolve(__dirname, 'src')
-      }
-    }
-  };
-
-  //development configration
-  if (isServe) {
-    Object.assign(config, {
-      server: {
-        open: true,
-        port: 3000,
-        strictPort: true,
-        hmr: {
-          clientPort: 3000, // Ensure the client connects to the correct port
-          overlay: false // Disable the server error overlay if desired
-        }
-      }
-    });
-  }
-
-  //production configration
-  if (isBuild) {
-    Object.assign(config, {
-      build: {
-        target: 'esnext',
-        outDir: 'dist',
-        assetsDir: 'static',
-        sourcemap: false,
-        manifest: false
-      },
+   //common configration for serve && build
+   const config = {
+      base: '/',
+      clearScreen: true,
       plugins: [
-        ...config.plugins,
-        visualizer({
-          template: 'network', //sunburst network
-          open: true,
-          gzipSize: true,
-          brotliSize: true,
-          filename: './build/analyse.html'
-        })
-      ]
-    });
-  }
+         react(),
+         jsconfigPaths(),
+         ViteEslint(),
+         timeReporter(),
+         Inspect(),
+         VitePWA({
+            registerType: 'prompt',
+            manifest: {
+               name: 'setup',
+               short_name: 'ssp',
+               description: 'Kathmandu Nepal',
+               theme_color: '#ffffff',
+               icons: [
+                  {
+                     src: '1.png',
+                     sizes: '192x192',
+                     type: 'image/png'
+                  },
+                  {
+                     src: '2.png',
+                     sizes: '192x192',
+                     type: 'image/png'
+                  },
+                  {
+                     src: '3.png',
+                     sizes: '192x192',
+                     type: 'image/png'
+                  }
+               ]
+            }
+         })
+      ],
+      resolve: {
+         alias: {
+            '@src': path.resolve(__dirname, 'src')
+         }
+      }
+   };
 
-  return config;
+   //development configration
+   if (isServe) {
+      Object.assign(config, {
+         server: {
+            open: true,
+            port: 3000,
+            strictPort: true,
+            hmr: {
+               clientPort: 3000, // Ensure the client connects to the correct port
+               overlay: false // Disable the server error overlay if desired
+            }
+         }
+      });
+   }
+
+   //production configration
+   if (isBuild) {
+      Object.assign(config, {
+         build: {
+            target: 'esnext',
+            outDir: 'dist',
+            assetsDir: 'static',
+            sourcemap: false,
+            manifest: false
+         },
+         plugins: [
+            ...config.plugins,
+            visualizer({
+               template: 'network', //sunburst network
+               open: true,
+               gzipSize: true,
+               brotliSize: true,
+               filename: './build/analyse.html'
+            })
+         ]
+      });
+   }
+
+   return config;
 });
