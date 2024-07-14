c952f89b93a42c443604fa3819203dc33d62c303
Manish Pokhrel <pokhrelmanish@gmail.com>
2024-07-14 13:21:35 +0545

    ongoing setup issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>
+// third party
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+// project imports
+import App from './App';
+import { store, persistor } from '@store/reducer';
+
+// google-fonts
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/700.css';
+
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+
+// style + assets
+import 'assets/scss/style.scss';
+
+// Get the root container element
+const container = document.getElementById('root');
+const root = createRoot(container);
+// ==============================|| REACT DOM RENDER  ||============================== //
+
+// Render the application wrapped with Redux Provider
+root.render(
+   <Provider store={store}>
+      <div> Hello</div>
+      <PersistGate loading={null} persistor={persistor}>
+         <App />
+      </PersistGate>
+   </Provider>
 );
---
+++ src/redux/store/reducer.js
@@ -0,0 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import storageSession from 'redux-persist/lib/storage/session';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+
+// project imports
+import customizationSlice from '@slice/customizationSlice';
+
+// Combine reducers
+const rootReducer = combineReducers({
+   customization: customizationSlice //Reducer that manages state not to be persisted
+});
+
+// Configure persistence
+const persistConfig = {
+   key: 'root', // Key for the persistor in storage
+   version: 1, // Version of your persisted state
+   storage: storageSession, // Storage engine to use (e.g., localStorage)
+   blacklist: ['customization'] // Array of state keys to blacklist from persistence
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store
+export const store = configureStore({
+   reducer: persistedReducer,
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+         serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            ignoredPaths: ['customization']
+         }
+      })
+});
+
+export const persistor = persistStore(store);
