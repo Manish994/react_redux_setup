7b2124149c3e5f5a71de90ffa5a86b26004dd74f
Manish Pokhrel <pokhrelmanish@gmail.com>
2024-07-15 11:39:21 +0545

    reaching near to final_part

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,66 +1,41 @@
-import { useEffect, useState } from 'react';
-
 // material-ui
 import Grid from '@mui/material/Grid';
 
-// project imports
-import EarningCard from './EarningCard';
-import PopularCard from './PopularCard';
-import TotalOrderLineChartCard from './TotalOrderLineChartCard';
-import TotalIncomeDarkCard from './TotalIncomeDarkCard';
-import TotalIncomeLightCard from './TotalIncomeLightCard';
-import TotalGrowthBarChart from './TotalGrowthBarChart';
-
-import { gridSpacing } from 'store/constant';
-
-// assets
-import StorefrontTwoToneIcon from '@mui/icons-material/StorefrontTwoTone';
+// Redux-related imports
+import { selectGridSpacing } from '@slice/customizationSlice';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
 const Dashboard = () => {
-   const [isLoading, setLoading] = useState(true);
-
-   useEffect(() => {
-      setLoading(false);
-   }, []);
-
    return (
-      <Grid container spacing={gridSpacing}>
+      <Grid container spacing={selectGridSpacing}>
          <Grid item xs={12}>
-            <Grid container spacing={gridSpacing}>
+            <Grid container spacing={selectGridSpacing}>
                <Grid item lg={4} md={6} sm={6} xs={12}>
-                  <EarningCard isLoading={isLoading} />
+                  <p>Hello</p>
                </Grid>
                <Grid item lg={4} md={6} sm={6} xs={12}>
-                  <TotalOrderLineChartCard isLoading={isLoading} />
+                  <p>Hello</p>
                </Grid>
                <Grid item lg={4} md={12} sm={12} xs={12}>
-                  <Grid container spacing={gridSpacing}>
+                  <Grid container spacing={selectGridSpacing}>
                      <Grid item sm={6} xs={12} md={6} lg={12}>
-                        <TotalIncomeDarkCard isLoading={isLoading} />
+                        <p>Hello</p>
                      </Grid>
                      <Grid item sm={6} xs={12} md={6} lg={12}>
-                        <TotalIncomeLightCard
-                           {...{
-                              isLoading: isLoading,
-                              total: 203,
-                              label: 'Total Income',
-                              icon: <StorefrontTwoToneIcon fontSize="inherit" />
-                           }}
-                        />
+                        <p>Hello</p>
                      </Grid>
                   </Grid>
                </Grid>
             </Grid>
          </Grid>
          <Grid item xs={12}>
-            <Grid container spacing={gridSpacing}>
+            <Grid container spacing={selectGridSpacing}>
                <Grid item xs={12} md={8}>
-                  <TotalGrowthBarChart isLoading={isLoading} />
+                  <p>Hello</p>
                </Grid>
                <Grid item xs={12} md={4}>
-                  <PopularCard isLoading={isLoading} />
+                  <p>Hello</p>
                </Grid>
             </Grid>
          </Grid>
