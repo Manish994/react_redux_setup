1023468b35d27d34e6b719229cc08b8b3e02e829
Manish Pokhrel <pokhrelmanish@gmail.com>
2024-07-17 16:46:30 +0545

    ongoing profile section

--- src/views/AccountProfile/profile.jsx
+++ src/views/AccountProfile/profile.jsx
@@ -4,6 +4,10 @@ import Grid from '@mui/material/Grid';
 import MuiTypography from '@mui/material/Typography';
 import Typography from '@mui/material/Typography';
 import Avatar from 'ui-component/extended/Avatar';
+import { CardContent, List, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import EmailIcon from '@mui/icons-material/Email';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 // project imports
 import SubCard from 'ui-component/cards/SubCard';
@@ -16,7 +20,7 @@ const Profile = () => {
       <Grid container spacing={customization.gridSpacing}>
          <Grid container item xs={12} md={12}>
             <Grid container spacing={2}>
-               <Grid item xs={12} sm={5}>
+               <Grid item xs={12} sm={4}>
                   <SubCard
                      title={
                         <Grid>
@@ -31,9 +35,55 @@ const Profile = () => {
                            </Grid>
                         </Grid>
                      }
-                  ></SubCard>
+                  >
+                     <CardContent>
+                        <List>
+                           <ListItemButton>
+                              <ListItemIcon>
+                                 <EmailIcon />
+                              </ListItemIcon>
+                              <ListItemText>
+                                 <Typography variant="subtitle1">Email</Typography>
+                              </ListItemText>
+                              <div>
+                                 <Typography variant="subtitle2" align="right">
+                                    demo@sample.com
+                                 </Typography>
+                              </div>
+                           </ListItemButton>
+                           <Divider />
+                           <ListItemButton>
+                              <ListItemIcon>
+                                 <SmartphoneIcon />
+                              </ListItemIcon>
+                              <ListItemText>
+                                 <Typography variant="subtitle1">Phone</Typography>
+                              </ListItemText>
+                              <div>
+                                 <Typography variant="subtitle2" align="right">
+                                    (+99) 9999 999 999
+                                 </Typography>
+                              </div>
+                           </ListItemButton>
+                           <Divider />
+                           <ListItemButton>
+                              <ListItemIcon>
+                                 <LocationOnIcon />
+                              </ListItemIcon>
+                              <ListItemText>
+                                 <Typography variant="subtitle1">Location</Typography>
+                              </ListItemText>
+                              <div>
+                                 <Typography variant="subtitle2" align="right">
+                                    Melbourne
+                                 </Typography>
+                              </div>
+                           </ListItemButton>
+                        </List>
+                     </CardContent>
+                  </SubCard>
                </Grid>
-               <Grid item xs={12} sm={7}>
+               <Grid item xs={12} sm={8}>
                   <SubCard title="Sub title">
                      <Grid container direction="column" spacing={1}>
                         <Grid item>
