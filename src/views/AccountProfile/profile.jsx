628891c24b144f8c36a8f161b3f2b0cb25601916
Manish Pokhrel <pokhrelmanish@gmail.com>
2024-07-18 12:17:13 +0545

    small part is done

--- src/views/AccountProfile/profile.jsx
+++ src/views/AccountProfile/profile.jsx
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Grid';
 import MuiTypography from '@mui/material/Typography';
 import Typography from '@mui/material/Typography';
 import Avatar from 'ui-component/extended/Avatar';
-import { CardContent, List, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import { List, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 import SmartphoneIcon from '@mui/icons-material/Smartphone';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -20,70 +20,64 @@ const Profile = () => {
       <Grid container spacing={customization.gridSpacing}>
          <Grid container item xs={12} md={12}>
             <Grid container spacing={2}>
-               <Grid item xs={12} sm={4}>
+               <Grid item xs={12} sm={5}>
                   <SubCard
                      title={
-                        <Grid>
-                           <Grid container spacing={2} alignItems="center">
-                              <Grid item>
-                                 <Avatar alt="User 1" src="/assets/avatar-3-DAakmaVf.png" />
-                              </Grid>
-                              <Grid item xs>
-                                 <Typography variant="subtitle1">Abinash Dhakal</Typography>
-                                 <Typography variant="subtitle2">Red Hacker</Typography>
-                              </Grid>
+                        <Grid container spacing={2} alignItems="center">
+                           <Grid item>
+                              <Avatar alt="User 1" src="/assets/avatar-3-DAakmaVf.png" />
+                           </Grid>
+                           <Grid item xs>
+                              <Typography variant="subtitle1">Abinash Dhakal</Typography>
+                              <Typography variant="subtitle2">Red Hacker</Typography>
                            </Grid>
                         </Grid>
                      }
                   >
-                     <CardContent>
-                        <List>
-                           <ListItemButton>
-                              <ListItemIcon>
-                                 <EmailIcon />
-                              </ListItemIcon>
-                              <ListItemText>
+                     <Grid container direction="column">
+                        <Grid item>
+                           <List>
+                              <ListItemButton>
+                                 <ListItemIcon>
+                                    <EmailIcon />
+                                 </ListItemIcon>
                                  <Typography variant="subtitle1">Email</Typography>
-                              </ListItemText>
-                              <div>
-                                 <Typography variant="subtitle2" align="right">
-                                    demo@sample.com
-                                 </Typography>
-                              </div>
-                           </ListItemButton>
-                           <Divider />
-                           <ListItemButton>
-                              <ListItemIcon>
-                                 <SmartphoneIcon />
-                              </ListItemIcon>
-                              <ListItemText>
+                                 <ListItemText>
+                                    <MuiTypography variant="subtitle2" align="right">
+                                       demo@sample.com
+                                    </MuiTypography>
+                                 </ListItemText>
+                              </ListItemButton>
+                              <Divider />
+                              <ListItemButton>
+                                 <ListItemIcon>
+                                    <SmartphoneIcon />
+                                 </ListItemIcon>
                                  <Typography variant="subtitle1">Phone</Typography>
-                              </ListItemText>
-                              <div>
-                                 <Typography variant="subtitle2" align="right">
-                                    (+99) 9999 999 999
-                                 </Typography>
-                              </div>
-                           </ListItemButton>
-                           <Divider />
-                           <ListItemButton>
-                              <ListItemIcon>
-                                 <LocationOnIcon />
-                              </ListItemIcon>
-                              <ListItemText>
+                                 <ListItemText>
+                                    <MuiTypography variant="subtitle2" align="right">
+                                       (+99) 9999 999 999
+                                    </MuiTypography>
+                                 </ListItemText>
+                              </ListItemButton>
+                              <Divider />
+                              <ListItemButton>
+                                 <ListItemIcon>
+                                    <LocationOnIcon />
+                                 </ListItemIcon>
                                  <Typography variant="subtitle1">Location</Typography>
-                              </ListItemText>
-                              <div>
-                                 <Typography variant="subtitle2" align="right">
-                                    Melbourne
-                                 </Typography>
-                              </div>
-                           </ListItemButton>
-                        </List>
-                     </CardContent>
+                                 <ListItemText>
+                                    <MuiTypography variant="subtitle2" align="right">
+                                       Melbourne
+                                    </MuiTypography>
+                                 </ListItemText>
+                              </ListItemButton>
+                           </List>
+                        </Grid>
+                     </Grid>
                   </SubCard>
                </Grid>
-               <Grid item xs={12} sm={8}>
+               <Grid item xs={12} sm={7}>
                   <SubCard title="Sub title">
                      <Grid container direction="column" spacing={1}>
                         <Grid item>
