bd84548c656b68c4de6d24f44e1f9c2d4f7f0cb0
Manish Pokhrel <pokhrelmanish@gmail.com>
2024-07-19 17:08:26 +0545

    image part is done

--- src/views/AccountProfile/personalDetails.jsx
+++ src/views/AccountProfile/personalDetails.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import { Avatar, Box, FormControl, Grid, Stack, SvgIcon, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Grid, Stack, SvgIcon, Typography } from '@mui/material';
 import CameraIcon from '@mui/icons-material/CameraAlt'; // or use any icon from MUI icons
 
 const PersonalDetails = () => {
    return (
       <Grid container>
          <Grid item xs={12}>
-            <Stack>
-               <label htmlFor="change-avatar">
-                  <Stack direction="row" alignItems="center">
-                     <Avatar alt="Avatar 1" src="/assets/default-CgtsF9c7.png" sx={{ width: 56, height: 56 }} />
-                     <Box ml={2}>
-                        <Stack direction="row" alignItems="center">
-                           <SvgIcon component={CameraIcon} sx={{ color: 'rgb(245, 245, 245)', fontSize: '1.5rem' }} />
-                           <Typography variant="caption" ml={1}>
-                              Upload
-                           </Typography>
-                        </Stack>
-                     </Box>
+            <Box position="relative" display="inline-block" sx={{ cursor: 'pointer' }}>
+               <Avatar alt="Avatar 1" src="/assets/default-CgtsF9c7.png" sx={{ width: 120, height: 120 }} />
+               <Box
+                  sx={{
+                     position: 'absolute',
+                     top: 0,
+                     left: 0,
+                     width: '100%',
+                     height: '100%',
+                     bgcolor: 'rgba(0, 0, 0, 0.5)',
+                     borderRadius: '50%',
+                     display: 'flex',
+                     alignItems: 'center',
+                     justifyContent: 'center',
+                     opacity: 0,
+                     transition: 'opacity 0.3s',
+                     '&:hover': {
+                        opacity: 1
+                     }
+                  }}
+               >
+                  <Stack direction="column" alignItems="center" spacing={0.5}>
+                     <SvgIcon component={CameraIcon} sx={{ color: 'rgb(245, 245, 245)', fontSize: '2rem' }} />
+                     <Typography variant="caption" sx={{ color: 'rgb(245, 245, 245)' }}>
+                        Upload
+                     </Typography>
                   </Stack>
-               </label>
-               <FormControl>
-                  <TextField id="change-avatar" type="file" variant="outlined" placeholder="Outlined" sx={{ display: 'none' }} />
-               </FormControl>
-            </Stack>
+               </Box>
+               <input type="file" id="change-avatar" style={{ display: 'none' }} />
+            </Box>
          </Grid>
       </Grid>
    );
