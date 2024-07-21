9650bd21fe98431b2a9010f6e834655dece21cdf
Manish Pokhrel <pokhrelmanish@gmail.com>
2024-07-21 16:20:56 +0545

    final_is_also_doen

--- src/views/AccountProfile/myAccount.jsx
+++ src/views/AccountProfile/myAccount.jsx
@@ -1,9 +1,52 @@
 import { useSelector } from 'react-redux';
-import { Grid, Table, TableBody, TableCell, TableContainer, TableRow, Paper, tableCellClasses } from '@mui/material';
+import {
+   Grid,
+   Table,
+   TableBody,
+   TableCell,
+   TableContainer,
+   TableRow,
+   Paper,
+   tableCellClasses,
+   Box,
+   Typography,
+   List,
+   ListItem,
+   ListItemIcon,
+   ListItemText
+} from '@mui/material';
 import MuiTypography from '@mui/material/Typography';
+import DesktopMacIcon from '@mui/icons-material/DesktopMac';
+import TabletIcon from '@mui/icons-material/Tablet';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
 // project imports
 import SubCard from 'ui-component/cards/SubCard';
+
+const devices = [
+   {
+      type: 'Cent Desktop',
+      address: '4351 Deans Lane, Chelmsford',
+      status: 'Current Active',
+      lastActive: null,
+      icon: <DesktopMacIcon />
+   },
+   {
+      type: 'Imho Tablet',
+      address: '4185 Michigan Avenue',
+      status: 'Inactive',
+      lastActive: '5 days ago',
+      icon: <TabletIcon />
+   },
+   {
+      type: 'Albs Mobile',
+      address: '3462 Fairfax Drive, Montcalm',
+      status: 'Inactive',
+      lastActive: '1 month ago',
+      icon: <SmartphoneIcon />
+   }
+];
 
 const MyAccount = () => {
    const customization = useSelector((state) => state.customization);
@@ -47,6 +90,13 @@ const MyAccount = () => {
                               </TableRow>
                               <TableRow>
                                  <TableCell component="th" scope="row">
+                                    <MuiTypography variant="subtitle1">KYC Status</MuiTypography>
+                                 </TableCell>
+                                 <TableCell>:</TableCell>
+                                 <TableCell>Male</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                 <TableCell component="th" scope="row">
                                     <MuiTypography variant="subtitle1">Approved By</MuiTypography>
                                  </TableCell>
                                  <TableCell>:</TableCell>
@@ -77,6 +127,35 @@ const MyAccount = () => {
                         </Table>
                      </TableContainer>
                   </Grid>
+               </SubCard>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+               <SubCard title="Advance Settings" sx={{ marginBottom: '24px' }}>
+                  <Box>
+                     <Typography variant="h6">RECOGNIZED DEVICES</Typography>
+                     <List>
+                        {devices.map((device, index) => (
+                           <ListItem key={index}>
+                              <ListItemIcon>{device.icon}</ListItemIcon>
+                              <ListItemText primary={device.type} secondary={device.address} />
+                              <Box display="flex" alignItems="center">
+                                 {device.status === 'Current Active' ? (
+                                    <>
+                                       <FiberManualRecordIcon color="success" />
+                                       <Typography variant="body2" color="textPrimary" sx={{ marginLeft: '8px' }}>
+                                          {device.status}
+                                       </Typography>
+                                    </>
+                                 ) : (
+                                    <Typography variant="body2" color="textSecondary">
+                                       Active {device.lastActive}
+                                    </Typography>
+                                 )}
+                              </Box>
+                           </ListItem>
+                        ))}
+                     </List>
+                  </Box>
                </SubCard>
             </Grid>
          </Grid>
