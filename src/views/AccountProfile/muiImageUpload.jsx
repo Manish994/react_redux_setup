cd6c25bea2d9fc5ba8683035dfc445512652adda
Manish Pokhrel <pokhrelmanish@gmail.com>
2024-07-26 12:41:43 +0545

    done

--- src/views/AccountProfile/muiImageUpload.jsx
+++ src/views/AccountProfile/muiImageUpload.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+// React-Redux
 import { useSelector } from 'react-redux';
+
+// Material-UI
 import { Box, Grid, InputLabel, CardContent, Card, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+
+// Third-party libraries
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import 'react-photo-view/dist/react-photo-view.css';
 
 const MuiImageUpload = () => {
+   /* ╔════════════════════════════════════════════════════════╗
+      ║                      FORM SETUP                        ║
+      ╚════════════════════════════════════════════════════════╝ */
+   const customization = useSelector((state) => state.customization);
    let imageFile = '';
 
    const imagePreview =
       'iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEX09PTMzMzJycnPz8/d3d3V1dXi4uLo6Ojw8PDx8fH39/ft7e3Y2NjQ0NDp6enb29uHE20LAAACaklEQVR4nO3b6W6CQBhGYUTWD9T7v9uylLIN6jCk8Cbn+deEGo6DMOAYRQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIJyFiuzshLesStJAdVZdufEV38LFydkZm6w+IrBJrK86itkxgU1ifnaKmz363QvUvsbjmoNYdjuXPPMQz6R7lfLsGKeq3bd76LvfHwnFIXt0tOKYwjuF51kVtjMUbzqFVmR1/cpK30idwv7qH98yz0SVwvI+XP19JygqhY9xehMnXokihfl0/hZ77a5I4WM2zXz5DKJI4XwKvjHLNGeGRmE1L7w7N7fKeRLSKCy+KGwCnedZjcJofruXuo7SbpwdiRqFlk4D42y9rf0eyOtEjcL5BzFeb2rV5oRApNAmj6QcjyRs8g4sE0UKJ4nxemJq8yGeJ6oURpY/uic26frppy0uJvNEmcI2JM/yovlz8cxlGbhIFCrcsA6cX0/kC52Bt3hMlC90Bk5HUbzQPYL9KA6b6BXmk8/YZuCYqFdYj/f47wL/EtUKrR6/LXsfOCSKFbaBQ+KnwGa79sqpVWjp7x1Ec6B+DhQsHAK7xM+BeoVjYLPzr499eoXTwO+IFfoHihXuWbWgVVh792kV7lt3IlRoe0ZQqvCLax+FZ8c4UUghheebFu6jU1gk++gU7l3t3f2rRmGAyxcGr329cuEh60stunBh2Z3y6yxM/wX52S1u/bf3Ryzzdq9tuIDnYWv1q7NTNlhy0O8t/Nb6/SfLbnHoYbpjSep/sjLfOZ0ZXfTXJKPgH69deAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABDyA0uAKIxQw0bjAAAAAElFTkSuQmCC';
 
+   /* ╔════════════════════════════════════════════════════════╗
+      ║                   IMAGE PREVIEW                        ║
+      ╚════════════════════════════════════════════════════════╝ */
    const UploadImageShow = () => (
       <Box>
          <PhotoProvider>
@@ -25,6 +36,9 @@ const MuiImageUpload = () => {
       </Box>
    );
 
+   /* ╔════════════════════════════════════════════════════════╗
+      ║               IMAGE UPLOAD BUTTON                      ║
+      ╚════════════════════════════════════════════════════════╝ */
    const UploadButton = () => (
       <Box>
          <InputLabel htmlFor="file-upload" style={{ display: 'block' }}>
@@ -49,7 +63,6 @@ const MuiImageUpload = () => {
       </Box>
    );
 
-   const customization = useSelector((state) => state.customization);
    return (
       <Grid container spacing={customization.gridSpacing} justifyContent="center" alignItems="center">
          <Grid item>
